f657d8a7c14628729cadafdc0f2063b7ccf2b60d
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-10-27 21:56:43 +0000

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -95,7 +95,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPorfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -111,7 +111,24 @@ let swiper = new Swiper('.portfolio__container', {
   });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    breakpoints {
+        568:{
+            slidesPerView: 2
+        }
+    }
+    
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
